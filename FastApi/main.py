509d4a8bb7699f1c7f1a222c8eac581d5195dad1
PhuocDo <phuocdn.1115@gmail.com>
2023-11-23 17:09:13 +0700

    Init project

---
+++ FastApi/main.py
@@ -0,0 +1,55 @@
+from fastapi import FastAPI, HTTPException, Depends
+from typing import Annotated, List
+from sqlalchemy.orm import Session
+from pydantic import BaseModel
+from database import SessionLocal, engine
+import models
+from fastapi.middleware.cors import CORSMiddleware
+
+myapp = FastAPI()
+
+origins = [
+    'http://localhost:3001'
+]
+
+myapp.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
+class LabelBase(BaseModel):
+    label: str
+    desc: str
+
+class LabelModel(LabelBase):
+    id: int
+    
+    class Config:
+        orm_mode = True
+        
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+models.Base.metadata.create_all(bind=engine)
+
+@myapp.post("/label/", response_model= LabelModel)
+async def create_label(label: LabelBase, db: db_dependency):
+    db_label = models.Label(**label.model_dump())
+    db.add(db_label)
+    db.commit()
+    db.refresh(db_label)
+    return db_label
+
+@myapp.get("/get_label/", response_model=List[LabelModel])
+async def get_label(db: db_dependency):
+    query = db.query(models.Label).offset(0).limit(100).all()
+    return query
